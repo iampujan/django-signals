6ce53160ef3c69b40d9825815251ce643fb01a7f
Pujan Thapa <iampujan@outlook.com>
2017-03-20 13:25:15 +0545

    regstered sites in admin.py as artists,rating and records

--- django-signals/home/admin.py
+++ django-signals/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Record,Rating,Artist
 
 # Register your models here.
+admin.site.register(Record)
+admin.site.register(Rating)
+admin.site.register(Artist)
--- django-signals/home/models.py
+++ django-signals/home/models.py
@@ -10,22 +10,37 @@ class Artist(models.Model):
 	name = models.ForeignKey(User)
 
 	def __str__(self):
-		return self.name
+		if self.name:
+			return str(self.name)
 
 
- class Record(models.Model):
- 	artist = models.ForeignKey('Artist')
- 	name = models.CharField()
- 	year = models.IntergerField()
+class Record(models.Model):
+ 	artist = models.ForeignKey(Artist)
+ 	name = models.CharField(max_length=20)
+ 	year = models.IntegerField()
 
  	def __str__(self):
  		return self.name
 
- class Rating(models.Model):
- 	record = models.ForeignKey('Record')
+class Rating(models.Model):
+ 	record = models.ForeignKey(Record)
  	user = models.ForeignKey(User)
- 	rating = models.IntergerField(default=0)
- 	expected_rating = models.IntergerField(default=0)
+ 	rating = models.IntegerField(default=0)
+ 	expected_rating = models.IntegerField(default=0)
 
  	def __str__(self):
  		return 'Record:{} and User:{}'.format(self.record,self.user)
+
+@receiver(post_save, sender=Record)
+def create_ratings_new_album(sender, instance, **kwargs):
+	if kwargs['created']:
+		users = User.objects.all()
+		for user in users:
+			Rating.objects.create(user=user, record=instance)
+
+@receiver(post_save, sender=User)
+def create_ratings_new_user(sender, instance, **kwargs):
+	if kwargs['created']:
+		records =Record.objects.all()
+		for record in records:
+			Rating.objects.create(user=instance, record=record)
